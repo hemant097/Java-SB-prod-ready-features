3a95c985d34dd51a7318f3fcb4915b487a3a911b
Hemant <hem789ant1997@outlook.com>
2026-01-29 00:00:10 +0530

    logging added log4j

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Learning-Java-production-ready-features
 
-### 1. Auditing features, JPA Auditing using AuditingEntityListener and AuditorAware, and Hibernate Envers
+#### 1. Auditing features, JPA Auditing using AuditingEntityListener and AuditorAware, and Hibernate Envers
 
+#### 2. RestClient, usage, fetching data from another REST API , handling error through RestClient
--- src/main/java/com/example/week4/postapplication/Clients/Impl/EmployeeClientImpl.java
+++ src/main/java/com/example/week4/postapplication/Clients/Impl/EmployeeClientImpl.java
@@ -5,6 +5,8 @@ import com.example.week4.postapplication.Clients.EmployeeClient;
 import com.example.week4.postapplication.DTO.EmployeeDTO;
 import com.example.week4.postapplication.Exceptions.ResourceNotFound;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -19,29 +21,42 @@ public class EmployeeClientImpl implements EmployeeClient {
 
     private final RestClient restClient;
 
+    Logger log = LoggerFactory.getLogger(EmployeeClientImpl.class);
+
 
     @Override
     public List<EmployeeDTO> getAllEmployees() {
-
+        log.trace("trying to retrieve employees in getAllEmployees");
         try {
+            log.info("Attempting to call the RestClient method in getAllEmployees");
             APIResponse<List<EmployeeDTO>> employeeDTOS = restClient
                     .get()
                     .uri("/all")
                     .retrieve()
+                    .onStatus(HttpStatusCode::is4xxClientError, (req,res)->{
+
+                        log.error(new String(res.getBody().readAllBytes()));
+
+                        throw new ResourceNotFound("could not find employees");
+                    })
                     .body(new ParameterizedTypeReference<>() {
                     });
-
             //using body() of RestClient to get the body from the ResponseEntity which is being returned
+
+            log.debug("successfully retrieved the employees in getAllEmployees");
+            log.trace("retrieved employee list in getAllEmployees : {}",employeeDTOS.getData());
 
             return employeeDTOS.getData();
         }
         catch (Exception e){
+            log.error("exception occurred in getAllEmployees",e);
             throw new RuntimeException(e);
         }
     }
 
     @Override
     public EmployeeDTO getEmployeeById(Long empId) {
+        log.trace("trying to get employees by id in getEmployeeById with empId :{}",empId);
         try {
             ResponseEntity<APIResponse<EmployeeDTO>> employeeDTOResponseEntity = restClient
                     .get()
@@ -55,6 +70,7 @@ public class EmployeeClientImpl implements EmployeeClient {
             return employeeDTOResponseEntity.getBody().getData();
         }
         catch (Exception e){
+            log.error("exception occurred in getEmployeeById :{}", e.getMessage());
             throw new RuntimeException(e);
         }
     }
